0f43fa12fea5534730e4feb3e1f07f49da8d75d3
MAKandCheese <miranda@karger.org>
2021-07-16 08:50:04 -0400

    added files

---
+++ public/scanning.js
@@ -0,0 +1,37 @@
+
+const webcamElement = document.getElementById('webcam');
+const canvasElement = document.getElementById('canvas');
+const snapSoundElement = document.getElementById('snapSound');
+const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
+
+window.addEventListener('load', function(){
+    webcam.start()
+    .then(result => {
+        console.log("webcam started");
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}, false);
+
+const capture = document.getElementById('captureBtn');
+capture.addEventListener('click', function () {
+    let picture = webcam.snap();
+    document.querySelector('#download-photo').href = picture;
+    webcam.stop();
+    document.getElementById('webcam').style.display = "none";
+}, false);
+
+
+/*const translate = document.getElementById('btn5');
+translate.addEventListener('click', function(){
+    var imageUrl = document.getElementById('download-photo').value;
+    Tesseract.recognize(
+        imageUrl,
+        'eng',
+        { logger: m => console.log(m) }
+    ).then(({ data: { text } }) => {
+        document.getElementById("output_container").innerHTML += "<p>" + text + "</p>";
+        console.log(text);
+    })
+}, false);*/
