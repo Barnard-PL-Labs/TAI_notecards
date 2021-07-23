c5320dd6c04b258cc7e17dc62f7237ec65ba41af
Mark Santolucito <mark.santolucito@yale.edu>
2021-07-23 11:00:46 -0400

    gettign gpt3 working - still not there

--- server/serverside_gpt3.js
+++ server/serverside_gpt3.js
@@ -30,23 +30,6 @@ async function runGPT3(myPromt) {
   });
 };
 
-(async () => {
-  const gptResponse = await openai.complete({
-    engine: 'davinci',
-    prompt: 'this is a test',
-    maxTokens: 5,
-    temperature: 0.9,
-    topP: 1,
-    presencePenalty: 0,
-    frequencyPenalty: 0,
-    bestOf: 1,
-    n: 1,
-    stream: false,
-    stop: ['\n', "testing"]
-  });
-
-  console.log(gptResponse.data);
-})();
 
 
 exports.runGPT3 = runGPT3;
